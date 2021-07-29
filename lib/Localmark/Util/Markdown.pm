cc2144152daf27247e41439a8e72c514634f4aac
bit4bit <bit4bit@riseup.net>
2021-07-29 03:19:42 +0000

    se adiciona procesamiento de fence blocks de markdown, y se implementa fence block plantuml, el cual si el comando esta instalado se renderiza en svg y se presenta al usuario
    FossilOrigin-Name: 9a016cdbab4d71d45b1a0553cbf571e040538c80a395efd09c6792f5cd148402

--- lib/Localmark/Util/Markdown.pm
+++ lib/Localmark/Util/Markdown.pm
@@ -1,16 +1,92 @@
 package Localmark::Util::Markdown;
+use v5.28;
 
 use Text::Markdown ();
 
+use Localmark::Util::File::Slurp qw( read_text );
+use File::Temp qw( tempfile tempdir );
+use File::Basename qw( basename );
+use Carp;
+use Data::Dumper;
+
 require Exporter;
 our @ISA = qw( Exporter );
-our @EXPORT = qw( markdown );
+our @EXPORT = qw( markdown plantuml_fence_block);
 
+
+my $has_plantuml = system('plantuml -version > /dev/null') == 0;
 
 sub markdown {
-    my ($text) = @_;
-    
+    my ($text, %args) = @_;
+
+    my $processors_fence_blocks = $args{fence_blocks} || {};
+    $text = process_fence_blocks($text, $processors_fence_blocks);
+
     return Text::Markdown::markdown($text);
 }
 
+sub plantuml_fence_block {
+    my $content = shift;
+
+    if (! $has_plantuml) {
+        carp 'WARN: DETECTE FENCE BLOCK PLANTUML BUT NOT DETECTED COMMAND plantuml';
+        return $content;
+    }
+
+    my ($fh, $filename) = tempfile();
+
+    say $fh '@startuml';
+    say $fh $content;
+    say $fh '@enduml';
+
+    close $fh;
+
+    my $cmd = "plantuml -tsvg $filename";
+
+    carp "PLANTUML: $cmd";
+
+    system( $cmd ) == 0
+        or carp "PLANTUML_FENCE_BLOCK failed: $?";
+
+    my $svgname = $filename . '.svg';
+
+    my $xml = read_text( $svgname );
+    $xml =~ s/\<\?xml[^>]+\>//;
+
+    return $xml;
+}
+
+sub process_fence_blocks {
+    my ($text, $processors) = @_;
+
+
+    # adicionamos identificador ejemplo plantuml:<id>
+    my $id = 1;
+    $text =~ s/(~~~[ ]*[\w\d]+)/$1.":".($id++)/emxg;
+
+    # ejecutamos procesadores
+    my @outputs = ();
+    while ($text =~ /~~~(\N+)\n?\s*([^~]+)~~~/xg) {
+        my $fence_id = $1;
+        my $fence_body = $2;
+        chomp $fence_body;
+
+        $fence_id =~ s/[^\w:\d]//;
+        my $fence_name = $fence_id =~ s/:.*//r;        
+
+        if (exists $processors->{$fence_name}) {
+            my $output = $processors->{$fence_name}($fence_body);
+            push @outputs, [$fence_id, $output]
+        }
+    }
+
+    # sustituimos salida de procesadors
+    for my $item (@outputs) {
+        my ($fence_id, $fence_body) = $item->@*;
+
+        $text =~ s/~{3}\s*$fence_id[^~]+~{3}/$fence_body/s;
+    }
+    
+    return $text;
+}
 1;
